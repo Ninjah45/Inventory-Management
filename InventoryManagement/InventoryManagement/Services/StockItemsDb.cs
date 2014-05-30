457566e60cba417b69b79ebf0e76bfa60d996648
Ninjah45 <Ben.tehira@gmail.com>
2014-05-30 15:35:20 +1200

    Fixed Controller

--- InventoryManagement/InventoryManagement/Services/StockItemsDb.cs
+++ InventoryManagement/InventoryManagement/Services/StockItemsDb.cs
@@ -8,41 +8,41 @@ namespace InventoryManagement
 {
     public class StockItemsDb
     {
-        public Repository<StockItem> repo = new Repository<StockItem>();
+        public static Repository<StockItem> repo = new Repository<StockItem>();
 
         public StockItemsDb()
         {
 
         }
 
-        public void AddItem(StockItem item)
+        public static void AddItem(StockItem item)
         {
             repo.Add(item);
             repo.SaveChanges();
         }
 
-        public void RemoveItem(StockItem item)
+        public static void RemoveItem(StockItem item)
         {
             repo.Delete(item);
             repo.SaveChanges();
         }
 
-        public StockItem GetItem(int id)
+        public static StockItem GetItem(int id)
         {
             return repo.FindById(id);
         }
 
-        public List<StockItem> ListItems()
+        public static List<StockItem> ListItems()
         {
             return repo.GetAll().ToList();
         }
 
-        public int GetQuantity(int id)
+        public static int GetQuantity(int id)
         {
             return repo.FindById(id).Quantity;
         }
 
-        public void UpdateStock(int id, int adjust)
+        public static void UpdateStock(int id, int adjust)
         {
             var update = repo.FindById(id);
             update.Quantity += adjust;
--- InventoryManagement/InventoryManagement/View/ConsoleView.cs
+++ InventoryManagement/InventoryManagement/View/ConsoleView.cs
@@ -16,8 +16,8 @@ namespace InventoryManagement
         public void DisplayMenu()
         {
             Console.WriteLine("Adjust stock [ADJUST]");
-            Console.WriteLine("Enter new customer order [ORDER] customername");
-            Console.WriteLine("Show current stock [STOCK]");
+            Console.WriteLine("List Stock items [LIST]");
+            Console.WriteLine("Add new Stock item [ADD]");
             Console.WriteLine("");
         }
 
@@ -26,25 +26,32 @@ namespace InventoryManagement
             Console.WriteLine();
         }
 
-        public int ConfirmQuantity(int id)
+        public void ChangeQuantity(int id)
         {
-            Console.WriteLine("What's the new quantity for" + STOCKNAME + "?");
-            string quantity = Console.ReadLine();
-            int quantityInt;
-            int.TryParse(quantity, out quantityInt);
-            return quantityInt;
+            Console.WriteLine("What's the new quantity for" + StockItemsDb.GetItem(id).Name + "?");
+            int quantity = Convert.ToInt32(Console.ReadLine());
+            StockItemsDb.UpdateStock(id, quantity);
         }
 
-        public void ConfirmAdjustment(int stockid, int quantity)
+        public void ListStock()
         {
-            if (quantity > stockid.quantity)
+            var stockList = StockItemsDb.ListItems();
+            foreach(StockItem item in stockList)
             {
-                Console.WriteLine("You've added" + STOCKNAME + ". New total is " + quantity + ".");
-            }
-            if (quantity < stockid.quantity)
-            {
-                Console.WriteLine("You've removed" + STOCKNAME + ". New total is " + quantity + ".");
+                Console.WriteLine("{0}. {1} is ${2}. You have {3} available", item.Id , item.Name, item.Price, item.Quantity);
             }
         }
+
+        //public void ConfirmAdjustment(int stockid, int quantity)
+        //{
+        //    if (quantity > stockid.quantity)
+        //    {
+        //        Console.WriteLine("You've added" + STOCKNAME + ". New total is " + quantity + ".");
+        //    }
+        //    if (quantity < stockid.quantity)
+        //    {
+        //        Console.WriteLine("You've removed" + STOCKNAME + ". New total is " + quantity + ".");
+        //    }
+        //}
     }
 }
